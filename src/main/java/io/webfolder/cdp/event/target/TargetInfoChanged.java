0af3204924094d164e5cf5abc40c1876826505e0
Paine <paine1690@126.com>
2017-07-08 20:37:46 +0800

    add TargetTargetInfoChanged in Events

---
+++ src/main/java/io/webfolder/cdp/event/target/TargetInfoChanged.java
@@ -0,0 +1,22 @@
+package io.webfolder.cdp.event.target;
+
+import io.webfolder.cdp.annotation.Domain;
+import io.webfolder.cdp.annotation.EventName;
+import io.webfolder.cdp.type.target.TargetInfo;
+
+/**
+ * Issued when some information about a target has changed. This only happens between targetCreated and targetDestroyed
+ */
+@Domain("Target")
+@EventName("targetInfoChanged")
+public class TargetInfoChanged {
+    private TargetInfo targetInfo;
+
+    public TargetInfo getTargetInfo() {
+        return targetInfo;
+    }
+
+    public void setTargetInfo(TargetInfo targetInfo) {
+        this.targetInfo = targetInfo;
+    }
+}
