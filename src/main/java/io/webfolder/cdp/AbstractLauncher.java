b4d3ee5b431ef149b4f182e861384e4af29e3739
webfolderio <support@webfolder.io>
2018-06-03 16:30:39 +0300

    Fix build error

--- src/main/java/io/webfolder/cdp/AbstractLauncher.java
+++ src/main/java/io/webfolder/cdp/AbstractLauncher.java
@@ -18,20 +18,14 @@
 package io.webfolder.cdp;
 
 import static java.lang.String.format;
+import static java.lang.Thread.sleep;
 import static java.util.Collections.emptyList;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.ConnectException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import io.webfolder.cdp.exception.CdpException;
 import io.webfolder.cdp.session.SessionFactory;
-import static java.lang.Thread.sleep;
 
 abstract class AbstractLauncher {
 
@@ -105,9 +99,9 @@ abstract class AbstractLauncher {
         }
 
         int     retryCount = 0;
-        boolean connected  = ping();
+        boolean connected  = factory.ping();
 
-        while ( ! ( connected = ping() ) && retryCount < 50 ) {
+        while ( ! ( connected = factory.ping() ) && retryCount < 50 ) {
             try {
                 sleep(100);
             } catch (InterruptedException e) {
