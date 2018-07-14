22a3f7387588fa23c089e5e652b19db4386a9864
webfolderio <support@webfolder.io>
2018-07-14 13:56:38 +0300

    Use Chromium instead of Chrome for unit tests

--- src/test/java/io/webfolder/cdp/test/MouseMoveTest.java
+++ src/test/java/io/webfolder/cdp/test/MouseMoveTest.java
@@ -17,13 +17,18 @@
  */
 package io.webfolder.cdp.test;
 
+import static io.webfolder.cdp.ChromiumDownloader.getExecutable;
+import static io.webfolder.cdp.ChromiumDownloader.getLatestInstalledVersion;
 import static java.nio.file.Paths.get;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.nio.file.Path;
 import java.util.List;
 
 import org.junit.Test;
 
+import io.webfolder.cdp.ChromiumDownloader;
+import io.webfolder.cdp.ChromiumVersion;
 import io.webfolder.cdp.Launcher;
 import io.webfolder.cdp.session.Session;
 import io.webfolder.cdp.session.SessionFactory;
@@ -33,21 +38,25 @@ public class MouseMoveTest {
 	@Test
 	@SuppressWarnings({ "unchecked" })
 	public void testMouseMove() throws Exception {
-		String path = get("src/test/resources/mouse-move.html").toAbsolutePath().toUri().toString();
+        ChromiumDownloader downloader = new ChromiumDownloader();
+        ChromiumVersion latest = getLatestInstalledVersion();
+        Path path = latest != null ? getExecutable(latest) : downloader.download();
+
+		String uri = get("src/test/resources/mouse-move.html").toAbsolutePath().toUri().toString();
 
 		Launcher launcher = new Launcher();
 
-		try (SessionFactory factory = launcher.launch(); Session session = factory.create()) {
+		try (SessionFactory factory = launcher.launch(path); Session session = factory.create()) {
 			session.enableConsoleLog();
-			session.navigate(path);
+			session.navigate(uri);
 			session.move(20, 20);
 			session.move(21, 21);
 			session.move(22, 22);
 			session.move(23, 23);
 			List<Double> positionsX = session.getVariable("positionsX", List.class);
 			List<Double> positionsY = session.getVariable("positionsY", List.class);
-			assertEquals(4, positionsX.size());
-            assertEquals(4, positionsY.size());
+            assertTrue(positionsX.size() >= 4);
+            assertTrue(positionsY.size() >= 4);
 		}
 	}
 }
