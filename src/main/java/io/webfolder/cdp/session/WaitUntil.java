300052a28cc7a9d42ff892bce603aa59a98da334
webfolderio <support@webfolder.io>
2018-07-31 19:05:50 +0300

    Added JavaDoc to WaitUntil.java [skip ci]

--- src/main/java/io/webfolder/cdp/session/WaitUntil.java
+++ src/main/java/io/webfolder/cdp/session/WaitUntil.java
@@ -18,15 +18,21 @@
 package io.webfolder.cdp.session;
 
 public enum WaitUntil {
-    Load("load"),
-    DomContentLoad("DOMContentLoaded"),
-    NetworkIdle("networkIdle"),
-    NetworkAlmostIdle("networkAlmostIdle"),
-    DomReady("DomReady");
+	Load("load"), DomContentLoad("DOMContentLoaded"),
+	/**
+	 * Consider navigation to be finished when there are no more than 0 network
+	 * connections for at least 500 ms.
+	 */
+	NetworkIdle("networkIdle"),
+	/**
+	 * Consider navigation to be finished when there are no more than 2 network
+	 * connections for at least 500 ms.
+	 */
+	NetworkAlmostIdle("networkAlmostIdle"), DomReady("DomReady");
 
-    public String value;
+	public String value;
 
-    private WaitUntil(String value) {
-        this.value = value;
-    }
+	private WaitUntil(String value) {
+		this.value = value;
+	}
 }
