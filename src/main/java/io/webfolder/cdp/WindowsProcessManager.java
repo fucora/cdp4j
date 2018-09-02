467a0a2ac9e47be8011f7eb189ee1cec2fbeaff6
webfolderio <support@webfolder.io>
2018-09-02 13:50:00 +0300

    retry to get cdp4jId more than once
    see: https://github.com/kohsuke/winp/blob/a7da800a46305ab25daae375d00c6d6895ab4d12/native/envvar-cmdline.cpp#L266

--- src/main/java/io/webfolder/cdp/WindowsProcessManager.java
+++ src/main/java/io/webfolder/cdp/WindowsProcessManager.java
@@ -18,7 +18,13 @@
  */
 package io.webfolder.cdp;
 
+import static java.lang.Thread.sleep;
+
+import java.util.TreeMap;
+
 import org.jvnet.winp.WinProcess;
+
+import io.webfolder.cdp.exception.CdpException;
 
 public class WindowsProcessManager extends ProcessManager {
 
@@ -30,7 +36,58 @@ public class WindowsProcessManager extends ProcessManager {
     void setProcess(CdpProcess process) {
         WinProcess winProcess = new WinProcess(process.getProcess());
         pid = winProcess.getPid();
-        cdp4jId = winProcess.getEnvironmentVariables().get("CDP4J_ID");
+        cdp4jId = getCdp4jId(winProcess, 20);
+        if (cdp4jId == null || cdp4jId.trim().isEmpty()) {
+            throw new CdpException("cdp4jId not found");
+        }
+    }
+
+    private String getCdp4jId(WinProcess process, int retryCount) {
+        String id = getCdp4jId(process);
+        if ( id != null ) {
+            return id;
+        } else {
+            for (int i = 0; i < retryCount; i++) {
+                id = getCdp4jId(process);
+                if ( id != null ) {
+                    return id;
+                } else {
+                    try {
+                        sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new CdpException(e);
+                    }
+                }
+            }
+        }
+        return id;
+    }
+
+    private String getCdp4jId(WinProcess process) {
+        String cdp4jId = null;
+        try {
+            TreeMap<String, String> variables = process.getEnvironmentVariables();
+            cdp4jId = variables.get("CDP4J_ID");
+        } catch (Throwable t) {
+            // ignore
+        }
+        if ( cdp4jId != null && ! cdp4jId.trim().isEmpty() ) {
+            return cdp4jId;
+        }
+        try {
+            String cmd = process.getCommandLine();
+            int start = cmd.indexOf("--cdp4jId=");
+            if (start > 0) {
+                int end = cmd.indexOf(" ", start + 1);
+                if (end == -1) {
+                    end = cmd.length();
+                }
+                cdp4jId = cmd.substring(start + "--cdp4jId=".length(), end);
+            }
+        } catch (Throwable t) {
+            return null;
+        }
+        return cdp4jId;
     }
 
     @Override
@@ -42,16 +99,22 @@ public class WindowsProcessManager extends ProcessManager {
         }
         try {
             WinProcess process = new WinProcess(pid);
-            String cdp4jId = process.getEnvironmentVariables().get("CDP4J_ID");
-            if (pid == process.getPid() &&
+            if (process.isRunning()) {
+                String cdp4jId = getCdp4jId(process, 20);
+                if (cdp4jId == null || cdp4jId.trim().isEmpty()) {
+                    throw new CdpException("cdp4jId not found");
+                }
+                if (pid == process.getPid() &&
                         this.cdp4jId.equals(cdp4jId)) {
-                process.killRecursively();
-                return true;
-            } else {
-                return false;
+                    process.killRecursively();
+                    return true;
+                } else {
+                    return false;
+                }
             }
         } catch (Throwable t) {
             return false;
         }
+        return false;
     }
 }
