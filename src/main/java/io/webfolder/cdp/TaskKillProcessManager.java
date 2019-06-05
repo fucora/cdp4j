86d88cc84516de7b2b5c72a4664bb67c79a2710e
webfolderio <support@webfolder.io>
2019-06-05 18:43:17 +0300

    Updated CHANGELOG.md Added missing taskkill to TaskKillProcessManager.java

--- src/main/java/io/webfolder/cdp/TaskKillProcessManager.java
+++ src/main/java/io/webfolder/cdp/TaskKillProcessManager.java
@@ -1,3 +1,21 @@
+/**
+ * cdp4j Commercial License
+ *
+ * Copyright 2017, 2019 WebFolder OÜ
+ *
+ * Permission  is hereby  granted,  to "____" obtaining  a  copy of  this software  and
+ * associated  documentation files  (the "Software"), to deal in  the Software  without
+ * restriction, including without limitation  the rights  to use, copy, modify,  merge,
+ * publish, distribute  and sublicense  of the Software,  and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  IMPLIED,
+ * INCLUDING  BUT NOT  LIMITED  TO THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL  THE AUTHORS  OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package io.webfolder.cdp;
 
 import static java.lang.String.valueOf;
@@ -28,10 +46,12 @@ public class TaskKillProcessManager extends ProcessManager {
             Process process = Runtime
                                 .getRuntime()
                                 .exec(new String[] {
-                                        "cmd", "/c", "/pid",
+                                        "cmd", "/c",
+                                        "taskkill",
+                                        "/pid",
                                         valueOf(winProcess.getPid()), "/T", "/F"
                                     });
-            return process.waitFor(10, SECONDS);
+            return process.waitFor(10, SECONDS) && process.exitValue() == 0;
         } catch (IOException | InterruptedException e) {
             throw new CdpException(e);
         }
