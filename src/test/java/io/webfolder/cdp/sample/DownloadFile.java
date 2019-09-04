706698683c4dd47d52435b530730174823354f57
webfolderio <support@webfolder.io>
2019-09-04 20:09:46 +0300

    Updated Downloader sample

--- src/test/java/io/webfolder/cdp/sample/DownloadFile.java
+++ src/test/java/io/webfolder/cdp/sample/DownloadFile.java
@@ -20,8 +20,9 @@ package io.webfolder.cdp.sample;
 
 import static io.webfolder.cdp.type.constant.DownloadBehavior.Allow;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import io.webfolder.cdp.Launcher;
 import io.webfolder.cdp.command.Page;
@@ -30,22 +31,24 @@ import io.webfolder.cdp.session.SessionFactory;
 
 public class DownloadFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Launcher launcher = new Launcher();
 
         try (SessionFactory factory = launcher.launch();
                             Session session = factory.create()) {
             session.navigate("https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html");
             session.waitDocumentReady();
-            session.getCommand().getNetwork().enable();
             Page page = session.getCommand().getPage();
-            Path downloadPath = Paths.get(".").toAbsolutePath();
-            page.setDownloadBehavior(Allow, downloadPath.toString());
+            Path tempDir = Files.createTempDirectory("download").toAbsolutePath();
+            page.setDownloadBehavior(Allow, tempDir.toString());
             // link must be visible before downloading the file
             session.evaluate("document.querySelector(\"code\").scrollIntoView()");
             // click the download link
             session.click("code");
-            session.wait(2000 * 200);
+            // There is an "Event.PageDownloadWillBegin" to detect if download is begin.
+            // Unfortunately DevTools protocol does not come with DownloadEnd event.
+            // It might better use a directory listener to detect if download is finished or not
+            session.wait(10000);
         } finally {
             launcher.kill();
         }
